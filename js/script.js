ceac58a915cd662e17c7702b1e2bc7acbcc2f11f
silvanoguel <silvanoguel@msn.com>
2023-03-22 14:43:50 +0100

    add:project completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
 const team = [
     {
         name: "Wayne Barnett",
@@ -34,22 +36,26 @@ const team = [
 
 const cardRow = document.querySelector(".row");
 
-
 let cards = "";
 for (let i = 0; i < team.length; i++) {
-    const curStudent = team[i];
+    const item = team[i];
+    // MILESTONE 1:
+    // Stampare su console le informazioni di nome, ruolo e la stringa della foto
+    console.log(item.name, item.role, item.photo) 
     cards += `
         <div class="col">
             <div class="card">
-            <img class="card-img-top" src="img/wayne-barnett-founder-ceo.jpg" alt="Card image cap">
+            <img class="card-img-top" src="img/${item.photo}" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">${curStudent.name}</h5>
+                    <h5 class="card-title">${item.name}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">
-                    ${curStudent.role}</h6>
+                    ${item.role}</h6>
                 </div>
             </div>
         </div>
     `;
 }
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
 console.log(cards);
 cardRow.innerHTML = cards;
